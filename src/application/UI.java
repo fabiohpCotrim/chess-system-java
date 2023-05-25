0ba0eb695ca2fc793490d1fa9dd9ba9a6f4995d3
Fabio Cotrim <fabiohenrique_p13@hotmail.com>
2023-05-25 10:21:17 -0300

    Possible moves of a piece

--- src/application/UI.java
+++ src/application/UI.java
@@ -32,7 +32,7 @@ public class UI {
 
 	// https://stackoverflow.com/questions/2979383/java-clear-the-console
 	public static void clearScreen() {
-		System.out.print("\033[H\033[2J.");
+		System.out.print("\033[H\033[2J");
 		System.out.flush();
 	}
 
